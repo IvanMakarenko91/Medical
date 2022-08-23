d298f03a672e6f52f032e3d9f12bfbe62aebf153
Makarenko Ivan <ivan.makarenko@free.fr>
2022-08-23 12:06:18 +0200

    JS Panneaux stylisés "Questions"

--- app.js
+++ app.js
@@ -6,3 +6,33 @@ toggle_menu.addEventListener('click', () => {
   toggle_menu.classList.toggle('active') ;
   menu.classList.toggle('responsive') ;
 })
+
+
+// Panneaux stylisés
+const allBlocs = document.querySelectorAll('.bloc');
+
+
+allBlocs.forEach(bloc => {
+    bloc.addEventListener('click', (e) => {
+
+        // console.log(e.target);
+        e.target.classList.add('activer');
+
+        for(let i = 0; i < allBlocs.length; i++ ){
+            if(allBlocs[i] !== e.target){
+                allBlocs[i].classList.remove('activer');
+            }
+        }
+        
+
+    })
+})
+
+// Boutton stop propagation des panneaux stilysés
+const questionsButtons = document.querySelectorAll('#questions button');
+
+questionsButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.stopPropagation();
+    })
+})
