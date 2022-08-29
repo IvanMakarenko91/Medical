8ad0f977d30534a4f88e95fad2a972f3d3a6b270
Makarenko Ivan <ivan.makarenko@free.fr>
2022-08-29 20:05:06 +0200

    Images + styles

--- app.js
+++ app.js
@@ -36,3 +36,21 @@ questionsButtons.forEach(btn => {
         e.stopPropagation();
     })
 })
+
+// Compteur automatique
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 1600;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(() => {
+    startValue += 2;
+    valueDisplay.style.fontSize = 4 + "rem";
+    valueDisplay.textContent = "+ " + startValue;
+    if(startValue == endValue){
+      clearInterval(counter);
+    }
+  }, duration);
+})
